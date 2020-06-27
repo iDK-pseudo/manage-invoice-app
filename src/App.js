462c35e580f64faeeead7e637ac3ea25f5185ccf
iDK-pseudo <shivamsharma151999@gmail.com>
2020-06-27 13:52:11 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import theme from '../src/utils/theme';
+import { withStyles} from '@material-ui/core/styles';
+import CollectorDashboard from '../src/views/CollectorDashboard';
+import CustomerDetails from '../src/views/CustomerDetails';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {ROLL_NUMBER} from '../src/utils/constants';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const styles = (theme) => ({
+  '@global': {
+    '*::-webkit-scrollbar': {
+      width: '0.4em',
+      height: '0.4em',
+    },
+    '*::-webkit-scrollbar-track': {
+      '-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.00)',
+    },
+    '*::-webkit-scrollbar-thumb': {
+      backgroundColor: '#6D7183',
+      outline: '1px solid slategrey',
+    },
+  },
+  mainBackground: {
+    background: theme.palette.primary.main,
+    height: '100vh',
+    width: '100vw',
+    overflow: 'hidden',
+  },
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+});
+class App extends Component {
+  render() {
+    console.log('theme', theme);
+    const { classes} = this.props;
+    return (
+      // <MuiThemeProvider theme={theme}>
+      <div className={classes.mainBackground}>
+        <Router basename={`/${ROLL_NUMBER}`}>
+          <Switch>
+            <Route exact path="/" component={CollectorDashboard} />
+            <Route path="/customer-dashboard" component={CustomerDetails} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withStyles(styles, { withTheme: true })(App);
